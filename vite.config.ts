587848e3844db98022b1bae963eb0077dd33bc84
Arne Küderle <a.kuederle@gmail.com>
2024-11-09 16:23:42 +0100

    Added custom viteplugin to inject metadata

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+// Plugin to inject meta tag and lang attribute into index.html
+const injectMetaTag = () => {
+  return {
+    name: 'inject-meta-tag',
+    transformIndexHtml(html: string) {
+      return html
+        .replace(
+          /<head>/,
+          `<head>\n<meta http-equiv="Content-Language" content="de">`
+        )
+        .replace(
+          /<html lang="en">/,
+          `<html lang="de">`
+        );
+    },
+  };
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), react(), injectMetaTag()],
   base: process.env.NODE_ENV === "production" ? "/billManager/" : "/",
   resolve: {
     alias: {
